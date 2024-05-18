d23be3ab194338c512f7cbca8c07c260bb8b7931
reqsanemustafayevaa <146015080+reqsanemustafayevaa@users.noreply.github.com>
2024-05-18 17:55:42 +0400

    databaserelationcreated

---
+++ examPrcCode/Exam.Core/Models/Doctor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Exam.Core.Models
+{
+    public class Doctor:BaseEntity
+    {
+        [Required]
+        [StringLength(65)]
+
+        public string Name { get; set; }
+        [Required]
+        [StringLength(50)]
+
+        public string Position { get; set; }
+        [Required]
+        [StringLength(100)]
+
+        public string TwitterUrl { get; set; }
+        [Required]
+        [StringLength(100)]
+
+        public string InstaUrl { get; set; }
+        [Required]
+        [StringLength(100)]
+
+
+        public string Fburl { get; set; }
+        [Required]
+        [StringLength(100)]
+
+
+        public string LNKUrl { get; set; }
+ 
+        [StringLength(100)]
+
+
+        public string? ImageUrl { get; set; }
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
+    }
+}
---
+++ examPrcCode/Exam.Data/DAL/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Exam.Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Data.DAL
+{
+    public class AppDbContext:IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options) 
+        {
+            
+        }
+        public DbSet<Doctor> Doctors { get; set; }
+    }
+}
